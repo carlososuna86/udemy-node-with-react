70ddaf3bc88487bd14108b472bd575e76150da14
Carlos Augusto Osuna <carlososuna86@gmail.com>
2021-06-13 17:26:02 -0400

    Fix 36: remove warnings from Mongoose connection

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(
+  keys.mongoURI, 
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    user: keys.mongoUser,
+    pass: keys.mongoPass,
+    dbName: keys.mongoDB,
+  }
+).catch(ex => {
+  console.log(ex);
+});
 
 const app = express();
 
